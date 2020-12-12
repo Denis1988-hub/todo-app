abef5d0e72dd5e8a505061b75f4268dbc6ad44c7
denis <you@example.com>
2020-12-12 21:52:37 +0200

    фильтр delete обновлен

--- src/components/TodoListItem/TodoListItem.js
+++ src/components/TodoListItem/TodoListItem.js
@@ -3,56 +3,58 @@ import classes from './TodoListItem.module.css'
 
 const TodoListItem = (props) => {
 
-    const clsDone = [
-        props.done ? classes.done : null
-    ];
+  const clsDone = [
+    props.done ? classes.done : null
+  ];
 
-    const doneIcon = props.done
-        ? <i className="far fa-calendar-check "/>
-        : <i className="fas fa-circle-notch"/>;
+  const doneIcon = props.done
+      ? <i className="far fa-calendar-check "/>
+      : <i className="fas fa-circle-notch"/>;
 
-    const importantIcon = props.important
-        ? <i className="fas fa-star"/>
-        : <i className="far fa-star"/>;
+  const importantIcon = props.important
+      ? <i className="fas fa-star"/>
+      : <i className="far fa-star"/>;
 
-    const allItems = (
-        <>
+  const allItems = (
+      <li className={classes.todoListItem}>
               <span>
                 {doneIcon}
             </span>
-            <span className={clsDone}
-                  onDoubleClick={props.onToggleDone}
-            >
+        <span className={clsDone}
+              onDoubleClick={props.onToggleDone}
+        >
                   {props.label}
         </span>
-            <span className={classes.remove}
-                  onClick={props.onDeleteTask}
-            >
+        <span className={classes.remove}
+              onClick={props.onDeleteTask}
+        >
               <i className="far fa-trash-alt"/>
           </span>
 
-            <span className={classes.important}
-                  onClick={props.onToggleImportant}
-            >
+        <span className={classes.important}
+              onClick={props.onToggleImportant}
+        >
                {importantIcon}
             </span>
-        </>
-    );
+      </li>
+  );
 
-    const deletedItems = (
-        <>
-            {props.label}
-            <span className={classes.deleted}>
-                <i className="fas fa-trash-restore" />
+  const deletedItems = (
+      <li className={classes.deleted}>
+        {props.label}
+        <span className={classes.deletedIcon}>
+                <i className="fas fa-trash-restore"/>
             </span>
-        </>
-    );
+      </li>
+  );
 
-    return (
-        <li className={classes.todoListItem}>
-            {allItems}
-        </li>
-    )
+
+  return(
+          props.ifDelete
+              ? deletedItems
+              : allItems
+  )
+
 };
 
 
